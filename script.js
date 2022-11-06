8bcb1beaf5e42753be4a264dc0f357c9bdba6bb5
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-11-07 00:06:33 +0100

    Start working on darkening feature

--- script.js
+++ script.js
@@ -43,7 +43,75 @@ function getRows() {
 
 
 function colorCell(event) {
-    event.target.classList.add('colored');
+    if (!(event.target.classList.contains('colored'))) {
+        event.target.style.backgroundColor = generateColor();
+        event.target.classList.add('colored');
+        console.log(generateColor());
+        console.log(event.target.style.backgroundColor);
+    } else {
+        const rgb = splitRGB(event.target.style.backgroundColor);
+        const hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
+        console.log(hsl);
+        console.log(returnUsableColor(hsl));
+        darkenedColor = darkenColor(hsl[0], hsl[1], hsl[2]);
+        console.log(darkenedColor);
+        event.target.style.backgroundColor = darkenedColor;
+    }
+}
+
+
+function generateColor() {
+    hue = Math.floor(Math.random() * 255);
+    sat = Math.floor(Math.random() * 100);
+    light = Math.floor(Math.random() * 100);
+    return `hsl(${hue}, ${sat}%, ${light}%)`
+}
+
+
+function splitRGB(rgb) {
+    console.log(rgb);
+    let newRGB = rgb.split(', ');
+    let newR = newRGB[0].slice(4);
+    let newG = newRGB[1];
+    let newB = newRGB[2].slice(0, newRGB[2].length - 1);
+    let finalRGB = [newR, newG, newB];
+    console.log(finalRGB);
+    return finalRGB;
+}
+
+
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = (max - min);
+        s = l >= 0.5 ? d / (2 - (max + min)) : d / (max + min);
+        switch(max){
+            case r: h = ((g - b) / d + 0)*60; break;
+            case g: h = ((b - r) / d + 2)*60; break;
+            case b: h = ((r - g) / d + 4)*60; break;
+        }
+    }
+
+    return [h, s, l];
+}
+
+function returnUsableColor(hsl) {
+    var str = '';
+        str += Math.round(hsl[0]) + ",";
+      str += Math.round(hsl[1]*100) + "%,";
+      str += Math.round(hsl[2]*100) + "%";
+      return str;
+    }
+
+
+function darkenColor (h, s, l) {
+    l = l * (1 - 10/100);
+    return `hsl(${h}, ${s}, ${l})`
 }
 
 
@@ -68,7 +136,7 @@ function promptDimension(direction) {
         alert('Invalid input, please specify a valid grid size between 1x1 and 100x100');
         return promptDimension(direction);
     } else if (newSize === null) {
-        return;
+        return null;
     } else {
         return newSize; 
     }
